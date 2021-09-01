7bdf855516aa4dbaa3e987cf27b7aa669484d340
shbair2013 <shbair2013@hotmail.comgit commit -m 'new project'git branch -M maingit config --global user.name shbair2013git config --global user.email shbair2013@hotmail>
2021-09-02 02:07:12 +0300

    for loop in java

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int j = 0; j <= 5; j++) {
+            for (int i = 0; i <= 5; i++) {
+                System.out.print("+");
+
+            }
+            System.out.println("");
+        }
+    }
+}
+
